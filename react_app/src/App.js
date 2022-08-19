cb1c687ac57e6d96ad911ce2c42ad9ac42228def
oookish <sonofmisery@inbox.ru>
2022-08-19 16:47:10 +0500

    Создал компоненты основных страниц сайта

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Home } from "./Home";
+import { Registration } from "./Registration";
+import { Map } from "./Map";
+import { Profile } from "./Profile";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PAGES = {
+  home: <Home/>,
+  registration: <Registration/>,
+  map: <Map/>,
+  profile: <Profile/>,
+}
+
+class App extends React.Component {
+  state = {currentPage: "home"};
+
+  navigateTo = (page) => {
+    this.setState( {currentPage: page});
+  };
+
+  render() {
+    return <>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <button onClick={() => {this.navigateTo("home")}}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => {this.navigateTo("registration")}}>Registration</button>
+          </li>
+          <li>
+            <button onClick={() => {this.navigateTo("map")}}>Map</button>
+          </li>
+          <li>
+            <button onClick={() => {this.navigateTo("profile")}}>Profile</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        {PAGES[this.state.currentPage]}
+      </section>
+    </main>
+    </>
+  }
 }
 
 export default App;
