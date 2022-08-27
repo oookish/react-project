ac4d35ad814453047dbc8fd4eab956726dd13d9c
oookish <sonofmisery@inbox.ru>
2022-08-28 01:06:45 +0500

    Выполнение задач второй недели

--- react_app/src/components/Profile.jsx
+++ react_app/src/components/Profile.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { withAuth } from "../AuthContex";
 
-export const Profile = () => {
-  return <>Profile</>;
-};
+export class Profile extends Component {
+  unauthenticate = (event) => {
+    event.preventDefault();
+    this.props.logOut();
+    this.props.navigate("home");
+  };
 
-export const ProfileWithAuth = withAuth(Profile)
+  goonmap = (event) => {
+    event.preventDefault();
+    this.props.navigate("map");
+  };
+
+  render() {
+    return (
+      <>
+        <p>
+          <button onClick={this.unauthenticate}>Выйти</button>
+        </p>
+        <p>
+          Перейти на карту
+          <button onClick={this.goonmap}>Карта</button>
+        </p>
+      </>
+    );
+  }
+}
+
+export const ProfileWithAuth = withAuth(Profile);
